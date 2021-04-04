b46c351ce7c8f00ec32e007a76c98a1b598da6d7
Artëm Zyrânov <netsuileo@gmail.com>
2021-04-04 12:43:56 +0300

    Basic looping experiment is successful.

---
+++ main.py
@@ -0,0 +1,59 @@
+import time
+import rtmidi
+from dataclasses import dataclass
+from collections import namedtuple
+from datetime import datetime, timedelta
+
+midiout = rtmidi.MidiOut()
+midiin = rtmidi.MidiIn()
+available_ports = midiout.get_ports()
+
+midiin.open_port(1)
+midiout.open_virtual_port("My virtual output")
+
+LOOP_LENGTH = timedelta(seconds=4)
+PRESSED = 144
+RELEASED = 128
+
+Message = namedtuple('Message', ['type', 'note', 'velocity'])
+
+@dataclass
+class DatedMessage:
+    message: Message
+    delta: timedelta
+
+last_note_pressed = None
+last_note_pressed_datetime = None
+
+with midiin:
+    with midiout:
+        loop = []
+        # wait for anything from keyboard
+        input()
+
+        listen_start = datetime.now()
+        while listen_start + LOOP_LENGTH > datetime.now():
+            raw_message = midiin.get_message()
+            if raw_message is not None:
+                message = Message(*raw_message[0])
+                print(message)
+                midiout.send_message(list(message))
+                delta = datetime.now() - listen_start
+                loop.append(DatedMessage(delta=delta, message=message)) 
+
+        print("-------------------------------")
+        print("LOOPING!")
+        print("-------------------------------")
+
+        while True:
+            play_start = datetime.now()
+            loop_index = 0
+            while play_start + LOOP_LENGTH > datetime.now():
+                if loop_index >= len(loop):
+                    continue
+
+                message = loop[loop_index]
+                if datetime.now() > play_start + message.delta:
+                    print(message.message)
+                    midiout.send_message(list(message.message))
+                    loop_index += 1
